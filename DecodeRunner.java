77ec41c82cfc7deb571a99c0d37dce26a87a4223
Darren Dawson <dwdawson@ucsc.edu>
2017-04-17 21:02:25 -0700

    Version 1 of huffman decoder

---
+++ DecodeRunner.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class DecodeRunner {
+  public static void main(String[] args) {
+
+    Scanner kb = new Scanner(System.in);
+    HuffDecoder decoder = new HuffDecoder();
+
+    //set up wordList
+    String filePath = "google-10000-english-usa.txt";
+    ArrayList<String> wordList = createWordList(filePath);
+
+    // DEV LOOP
+    while(true) {
+
+      System.out.print("Encoded Message: ");
+
+      String encoded = kb.nextLine();
+      String decoded = decoder.decode(encoded, wordList);
+      System.out.println("Decoded: " + decoded);
+    } // END DEV LOOP
+  }
+
+  //---createWordLIst----------------------------------------------------
+
+  // returns word list
+  public static ArrayList<String> createWordList(String filePath) {
+    ArrayList<String> wordMap = new ArrayList<String>();
+    
+    BufferedReader br = null;
+    FileReader fr = null;
+      
+    try {
+      fr = new FileReader(filePath);
+      br = new BufferedReader(fr);   
+      String line;	
+      br = new BufferedReader(new FileReader(filePath));
+      
+      while ((line = br.readLine()) != null) {
+	wordMap.add(line);
+      }  
+    } catch (IOException e) {      
+      e.printStackTrace();    
+    } finally {    
+      try {	  
+	if (br != null)
+	  br.close();	
+	if (fr != null)
+	  fr.close();	  
+      } catch (IOException ex) {
+	ex.printStackTrace();	  
+      }	
+    }   
+    return wordMap;
+  }
+
+}
